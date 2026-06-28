d3a556e6fe7b7c31e769e7cc764a8d7c566e08dc
atakeuchii <takeuchi.akihide1110@gmail.com>
2026-06-28 19:47:03 +0900

    feat: Implement in-memory key-value store

--- src/my_storage/core.clj
+++ src/my_storage/core.clj
@@ -1,6 +1,29 @@
-(ns my-storage.core)
+(ns my-storage.core
+  (:refer-clojure :exclude [get]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol KVStore
+  (-put [this k v])
+  (-get [this k])
+  (-delete [this k])
+  (-close [this]))
+
+(defrecord MemoryStore [data]
+  KVStore
+  (-put [this k v]
+    (swap! data assoc k v)
+    this)
+  (-get [this k]
+    (clojure.core/get @data k))
+  (-delete [this k]
+    (swap! data dissoc k)
+    this)
+  (-close [this] nil))
+
+(defn put [store k v] (-put store k v))
+(defn get [store k] (-get store k))
+(defn delete [store k] (-delete store k))
+(defn close [store] (-close store))
+
+(defn open 
+  []
+  (->MemoryStore (atom (sorted-map))))
--- test/my_storage/core_test.clj
+++ test/my_storage/core_test.clj
@@ -1,7 +1,31 @@
 (ns my-storage.core-test
-  (:require [clojure.test :refer :all]
-            [my-storage.core :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [my-storage.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest put-then-get
+  (let [db (core/open)]
+    (core/put db "k" "v")
+    (is (= "v" (core/get db "k")))))
+
+(deftest overwrite
+  (let [db (core/open)]
+    (core/put db "k" "v1")
+    (core/put db "k" "v2")
+    (is (= "v2" (core/get db "k")))))
+
+(deftest missing-key
+  (let [db (core/open)]
+    (is (nil? (core/get db "nope")))))
+
+(deftest delete-removes-key
+  (let [db (core/open)]
+    (core/put db "k" "v")
+    (core/delete db "k")
+    (is (nil? (core/get db "k")))))
+
+(deftest keys-are-sorted
+  (let [db (core/open)]
+    (doseq [k ["c" "a" "b" "delta" "alpha"]]
+      (core/put db k k))
+    (is (= ["a" "alpha" "b" "c" "delta"]
+           (keys @(:data db))))))
