8101f6608d41b9c7fc18766dbb7f7fd8436f00de
atakeuchii <takeuchi.akihide1110@gmail.com>
2026-07-21 23:46:43 +0900

    feat: modify version name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject atakeuchii/my-storage "0.1.0-SNAPSHOT"
+(defproject atakeuchii/my-storage (or (System/getenv "RELEASE_VERSION") "0.0.0-SNAPSHOT")
   :description "FIXME: write description"
   :url "https://github.com/atakeuchii/my-storage"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -15,4 +15,5 @@
   :deploy-repositories [["github"
                          {:url "https://maven.pkg.github.com/atakeuchii/my-storage"
                           :username :env/github_actor
-                          :password :env/github_token}]])
+                          :password :env/github_token
+                          :sign-releases false}]])
