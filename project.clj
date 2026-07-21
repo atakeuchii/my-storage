7adc2a47bab2fa3bca9d677bab741d7401bd39e0
atakeuchii <takeuchi.akihide1110@gmail.com>
2026-07-21 23:45:16 +0900

    fix: modify version name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject atakeuchii/my-storage "0.1.0-SNAPSHOT"
+(defproject atakeuchii/my-storage ~(or (System/getenv "RELEASE_VERSION") "0.0.0-SNAPSHOT")
   :description "FIXME: write description"
   :url "https://github.com/atakeuchii/my-storage"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
